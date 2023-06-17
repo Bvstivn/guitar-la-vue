73b450385095a1f91fe59e05a5bf11d196238779
Bvstivn <bbmmllvv@gmail.com>
2023-06-17 12:23:47 -0400

    avance

--- src/components/Guitarra.vue
+++ src/components/Guitarra.vue
@@ -1,17 +1,19 @@
 <template>
-  <div
-    class="col-md-6 col-lg-4 my-4 row align-items-center"
-  >
+  <div class="col-md-6 col-lg-4 my-4 row align-items-center">
     <div class="col-4">
-      <img class="img-fluid" src="/img/guitarra_01.jpg" alt="imagen guitarra" />
+      <img
+        class="img-fluid"
+        :src="'/img/' + guitarra.imagen + '.jpg'"
+        :alt="'imagen guitarra' + guitarra.nombre"
+      />
     </div>
     <div class="col-8">
       <h3 class="text-black fs-4 fw-bold text-uppercase">
         {{ guitarra.nombre }}
       </h3>
-      <p>A</p>
-      <p class="fw-black text-primary fs-3">$299</p>
-      <button type="button" class="btn btn-dark w-100">
+      <p>{{ guitarra.descripcion }}</p>
+      <p class="fw-black text-primary fs-3">${{ guitarra.precio }}</p>
+      <button @click="$emit('agregar-carrito', guitarra)" type="button" class="btn btn-dark w-100">
         Agregar al Carrito
       </button>
     </div>
@@ -20,10 +22,14 @@
 </template>
 
 <script setup>
+
 const props = defineProps({
-    guitarra: {
-        type: Object,
-        required: true,
-    }
+  guitarra: {
+    type: Object,
+    required: true,
+  },
 });
+
+defineEmits('agregar-carrito');
+
 </script>
